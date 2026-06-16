035e36f802cfde23ab3cfeabf310489d6e55682e
aziz <raximovaziz711@gmail.com>
2026-06-16 17:22:34 +0500

    fdgh

--- apps/urls.py
+++ apps/urls.py
@@ -9,13 +9,10 @@ urlpatterns = [
     path('login', LoginViewList.as_view(), name="login"),
     path('guest-add', GuestAddCreateView.as_view(), name='guest-add'),
     path('guest-list', GuestListView.as_view(), name='guest-add'),
-    path('vendordetail', VenderListView.as_view(), name="vendor-detail"),
-    path('vendorlist', VenderListView.as_view(), name="vendor-list"),
+    path('vendor-detail', VenderListView.as_view(), name="vendor-detail"),
+    path('vendor-list', VenderListView.as_view(), name="vendor-list"),
     path('tasklist', TaskViewList.as_view(), name="task-list"),
     path('Dashboard', DashboardViewList.as_view(), name="dashboard"),
-
-    path('tasklist' , TaskViewList.as_view(), name="task-list"),
-    path('Dashboard' , DashboardViewList.as_view(), name="dashboard"),
     path('invite-create', InviteCreateList.as_view(), name="invite-create"),
     path('invite-share', InviteShareView.as_view(), name="invite-share")
 ]
